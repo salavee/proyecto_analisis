a49fd7a07ca9b3ffc9964409ed8ad3e51f7def28
yymamania <yerson.mamani.a@gmail.com>
2021-02-16 08:17:02 -0500

    Update Almacen.h

--- Almacen.h
+++ Almacen.h
@@ -7,7 +7,6 @@ class producto;
 
 class Almacen{
 	private:
-		producto *Producto;
 		vector<producto*> ListaProductos;
 		vector<Factura*> ListaFacturas;
 	public:
@@ -17,19 +16,18 @@ class Almacen{
 		void visualizarFacturas();
 //		void visualizarProveedor(producto*);
 		void agregar_Producto(producto*);
-<<<<<<< HEAD
+//<<<<<<< HEAD
 		void agregar_Factura(Factura*);
 		void modificar_precio(string);
 		
-=======
+//=======
 //		void agregar_Factura(Factura*);
 		void buscar_Producto(producto*);
 		void eliminar_Producto(string);
->>>>>>> a113f3870ae809d1b2284e8bf295e890231060b5
+//>>>>>>> a113f3870ae809d1b2284e8bf295e890231060b5
 };
-
-	producto* Almacen::getProducto(){
-		return Producto;
+	
+	Almacen::Almacen(){		
 	}
 	
 	void Almacen::visualizarProductos(){
@@ -44,10 +42,7 @@ class Almacen{
 		}
 
 	}
-	
-	Almacen::Almacen(){		
-	}
-	
+		
 	void Almacen::visualizarFacturas(){
 		
 		if(ListaFacturas.size()>0){		
@@ -58,11 +53,11 @@ class Almacen{
 		else{
 			cout<<"Aun no hay Facturas registradas"<<endl;
 		}
-<<<<<<< HEAD
+//<<<<<<< HEAD
 	}
 	
-=======
-	}*/
+//=======
+//	}*/
 	void Almacen::eliminar_Producto(string _nombre)
 	{
 		int c=0;
@@ -89,7 +84,7 @@ class Almacen{
 			}	
 		}
 	}
->>>>>>> a113f3870ae809d1b2284e8bf295e890231060b5
+//>>>>>>> a113f3870ae809d1b2284e8bf295e890231060b5
 	void Almacen::agregar_Producto(producto *product){
 		ListaProductos.push_back(product);
 	}
@@ -101,19 +96,26 @@ class Almacen{
 	void Almacen::modificar_precio(string nombre){
 		vector<producto*>::iterator p = ListaProductos.begin();
 		
-		int i=0;
+		int i=0,band=0;
 		float precio_new;
-		while( p != ListaProductos.end() ){
+		
+		while( p != ListaProductos.end() ){			
 			if( ListaProductos[i]-> getnombre() == nombre){
+				
 				cout<<endl<<ListaProductos[i] -> getnombre()<<endl;
 				cout<<"Ingrese nuevo precio: ";cin>>precio_new;
 				
 				ListaProductos[i] -> setprecio_venta(precio_new);
 				
 				cout<<"\nModificacion guardada"<<endl;
+				band = 1;
 				break;
 			} 
 			i++;
+			p++;
+		}
+		if(band == 0){
+			cout<<"\nNo existe Producto"<<endl;
 		}
 	}
 	
