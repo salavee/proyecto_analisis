4ea84776e9fc59d73a0551e3f767a7284e4e05a3
salavee <salavee@users.noreply.github.com>
2021-02-24 16:28:32 -0500

    Update factu.h

--- factu.h
+++ factu.h
@@ -13,20 +13,8 @@ class Factura{
 		float saldo;
 	
 	public:
-		Factura();
+		Factura(int,int,string,string,string,string,float);
 		
-		//Establecer atributos
-/*		void setFactura(int _RUC, int _NumFactura, string _NombreE, string _DireccionE, string _fecha, string _telefono, float _saldo){
-			RUC = _RUC;
-			NumFactura = _NumFactura;
-			NombreE = _NombreE;
-			DireccionE = _DireccionE;
-			fecha = _fecha;
-			telefono = _telefono;
-			saldo = _saldo;
-		}	*/
-		
-		//Obtener atributos
 		int getRUC()	{  return RUC;  }		
 		int getNumFactura()	{  return NumFactura;  }	
 		string getNombreE()	{  return NombreE;  }	
@@ -36,28 +24,20 @@ class Factura{
 		float getsaldo()	{  return saldo;  }
 		
 		void MostrarFactura();	
-		void IngresarFactura();
 };
 
-Factura::Factura(){
-}
-
-void Factura::IngresarFactura(){
-	cout<<"\tIngresar Facturas";
-	cout<<"\nRUC: ";cin>>RUC;
-	cout<<"Numero Factura: ";cin>>NumFactura;
-	cout<<"Nombre de Empresa: ";cin>>NombreE;
-	fflush(stdin);
-	cout<<"Dirrecion de Empresa: ";cin>>DireccionE;
-	fflush(stdin);
-	cout<<"Fecha: ";cin>>fecha;
-	cout<<"Telefono: ";cin>>telefono;
-	cout<<"Saldo: ";cin>>saldo;
+Factura::Factura(int _ruc,int _NumFactura,string _NombreE,string _DireccionE,string _fecha,string _telefono,float _saldo){
+	RUC = _ruc;
+	NumFactura = _NumFactura;
+	NombreE = _NombreE;
+	DireccionE = _DireccionE;
+	fecha = _fecha;
+	telefono = _telefono;
+	saldo = _saldo;
 }
 
 void Factura::MostrarFactura(){
-	cout<<"\n\tRegistro de Facturas"<<endl;
-	cout<<"\nRUC: "<<RUC;
+	cout<<"RUC: "<<RUC;
 	cout<<"\nNumero Factura: "<<NumFactura;
 	cout<<"\nNombre de Empresa: "<<NombreE;
 	cout<<"\nDirrecion de Empresa: "<<DireccionE;
@@ -65,3 +45,4 @@ void Factura::MostrarFactura(){
 	cout<<"\nTelefono: "<<telefono;
 	cout<<"\nSaldo: "<<saldo;
 }
+
